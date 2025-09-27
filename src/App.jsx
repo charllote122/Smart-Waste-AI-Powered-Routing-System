07ae3c8bd705a7ecd989145485047ef841472469
rhodandwiga <wanjarhoda24@gmail.com>
2025-09-27 10:34:07 +0300

    v

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import FeaturesSection from './components/FeaturesSection'
+import WasteReportingApp from './components/WasteReportingApp'
+
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      
+      <WasteReportingApp />
+      <FeaturesSection />
+     
+
+  
+    </div>
+  )
+}
+
+export default App
